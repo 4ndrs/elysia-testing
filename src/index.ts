b9df565cd48f2a8a009db7522a88c54747334f21
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2024-06-28 10:19:15 +0200

    Use the internal Elysia.t schema builder

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,39 @@
-import { z } from "zod";
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+
+const userSchema = t.Object({
+  name: t.String(),
+  color: t.String(),
+});
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
-  .get("/user/:id", ({ params: { id } }) => {
-    console.log("received id:", id);
+  .get(
+    "/user/:id",
+    ({ params: { id } }) => {
+      console.log("received id:", id);
 
-    return `Hello user ${id}`;
-  })
-  .post("/user", ({ body }) => {
-    const parsedUser = UserSchema.safeParse(body);
+      return `Hello user ${id}`;
+    },
+    { params: t.Object({ id: t.Numeric() }) },
+  )
+  .post(
+    "/user",
+    ({ body }) => {
+      const user = { id: 1, ...body };
 
-    if (!parsedUser.success) {
-      const errors = parsedUser.error.flatten().fieldErrors;
-
-      console.log("received invalid user schema:", errors);
+      console.log("created user:", user);
 
       return new Response(
-        JSON.stringify({ message: "Invalid fields", errors }),
-        { status: 422 },
+        JSON.stringify({ message: "User created", data: user }),
+        {
+          status: 201,
+        },
       );
-    }
-
-    const user = { id: 1, ...parsedUser.data };
-
-    console.log("created user:", user);
-
-    return new Response(
-      JSON.stringify({ message: "User created", data: user }),
-      {
-        status: 201,
-      },
-    );
-  })
+    },
+    { body: userSchema },
+  )
   .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
-
-const UserSchema = z.object({
-  name: z.string(),
-  color: z.string(),
-});
